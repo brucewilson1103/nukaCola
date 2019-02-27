e6f9795bd982db0a306ac8013cf0319ce37f1b14
da Vinci <brucewilson1103@gmail.com>
2019-02-27 10:51:22 -0500

    initial push with files and server starter code

--- client/nukacola/src/Neo.js
+++ client/nukacola/src/Neo.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Neo from './Neo';
 
-class App extends Component {
+class Neo extends Component {
   render() {
     return (
-      <div id = "importNeo">
-          This is JSX
-
-        {<Neo/>}
-                      {/* <div className="jumbotron jumbotron-fluid bg-info text-light">
+      <div>
+                      <div className="jumbotron jumbotron-fluid bg-info text-light">
                     <div className="container-fluid">
                       <h2>Potentially Hazardous Near Earth Asteroids</h2>
                     </div>
@@ -36,7 +32,7 @@ class App extends Component {
                     </div>
                   </div>
                 </div>
-                 */}
+                
                 {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/js/bootstrap.min.js"></script>
@@ -46,4 +42,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Neo;
